01b884340af5ca6e255c03f9aaf28bff1c9ccd5b
dorigodev <murillodorigo@gmail.com>
2025-04-30 11:28:38 -0300

    ✨ Feat: add UserRepository

--- src/main/java/dev/dorigo/financecontrol/repository/UserRepository.java
+++ src/main/java/dev/dorigo/financecontrol/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package dev.dorigo.financecontrol.repository;
 
-public class UserRepository {
+import dev.dorigo.financecontrol.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<UserDetails> findByEmail(String email);
 }
