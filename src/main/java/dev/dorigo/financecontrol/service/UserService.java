7346a3c391b9d4e1e1ba36df58b2c18d8b8aaa41
dorigodev <murillodorigo@gmail.com>
2025-04-30 16:09:49 -0300

    ✨ Feat: create save function with password encryptation to user

--- src/main/java/dev/dorigo/financecontrol/service/UserService.java
+++ src/main/java/dev/dorigo/financecontrol/service/UserService.java
@@ -1,4 +1,22 @@
 package dev.dorigo.financecontrol.service;
 
+import dev.dorigo.financecontrol.domain.user.User;
+import dev.dorigo.financecontrol.domain.user.UserRole;
+import dev.dorigo.financecontrol.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User save(User user) {
+        String password = user.getPassword();
+        user.setPassword(passwordEncoder.encode(password));
+        return userRepository.save(user);
+    }
 }
