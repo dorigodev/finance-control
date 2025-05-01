617074f8d28a42f33811c77255d2310cb2b3a290
dorigodev <murillodorigo@gmail.com>
2025-05-01 12:52:44 -0300

    ✨ Feat: add function to get authenticated user by jwt token

--- src/main/java/dev/dorigo/financecontrol/service/AuthService.java
+++ src/main/java/dev/dorigo/financecontrol/service/AuthService.java
@@ -1,7 +1,9 @@
 package dev.dorigo.financecontrol.service;
 
+import dev.dorigo.financecontrol.domain.user.User;
 import dev.dorigo.financecontrol.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,5 +18,11 @@ public class AuthService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    public User getAuhenticatedUser(){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return (User) userRepository.findByEmail(email).orElseThrow();
+
     }
 }
