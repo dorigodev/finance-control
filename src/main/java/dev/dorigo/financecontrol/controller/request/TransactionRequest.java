ef9b43ad6b34cd4d010a0154ceea65f6d1614210
dorigodev <murillodorigo@gmail.com>
2025-05-07 10:04:53 -0300

    ♻️ Chore: add JsonFormat to date in DTO's

--- src/main/java/dev/dorigo/financecontrol/controller/request/TransactionRequest.java
+++ src/main/java/dev/dorigo/financecontrol/controller/request/TransactionRequest.java
@@ -1,10 +1,14 @@
 package dev.dorigo.financecontrol.controller.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import dev.dorigo.financecontrol.domain.transaction.Type;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record TransactionRequest(String description, BigDecimal amount, Type type,
+public record TransactionRequest(String description,
+                                 BigDecimal amount,
+                                 Type type,
+                                 @JsonFormat(pattern = "dd/MM/yyyy")
                                  LocalDate date) {
 }
--- src/main/java/dev/dorigo/financecontrol/controller/response/TransactionResponse.java
+++ src/main/java/dev/dorigo/financecontrol/controller/response/TransactionResponse.java
@@ -1,5 +1,6 @@
 package dev.dorigo.financecontrol.controller.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import dev.dorigo.financecontrol.domain.transaction.Type;
 import lombok.Builder;
 
@@ -7,6 +8,10 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Builder
-public record TransactionResponse(Long id,String description, BigDecimal amount, Type type,
+public record TransactionResponse(Long id,
+                                  String description,
+                                  BigDecimal amount,
+                                  Type type,
+                                  @JsonFormat(pattern = "dd/MM/yyyy")
                                   LocalDate date) {
 }
